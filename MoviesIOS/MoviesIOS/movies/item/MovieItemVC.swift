6e6fbff87e9298e6d8dc7ec8a14ef7c7f86d69a8
azharkova <azharkova@usetech.ru>
2021-01-25 01:10:50 +0700

    iOS other screens

---
+++ MoviesIOS/MoviesIOS/movies/item/MovieItemVC.swift
@@ -0,0 +1,22 @@
+//
+//  MovieItemVC.swift
+//  MoviesIOS
+//
+//  Created by Anna Zharkova on 25.01.2021.
+//
+
+import UIKit
+
+class MovieItemVC: UIViewController {
+
+    @IBOutlet weak var movieImageView: UIImageView!
+    @IBOutlet weak var titleLabelText: UILabel!
+    @IBOutlet weak var movieTextLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
